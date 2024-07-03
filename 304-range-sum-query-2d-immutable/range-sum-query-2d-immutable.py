72b1a20a895f679fae579b755fd874c03dd1a076
Pavan Kumar <32418907+raven610@users.noreply.github.com>
2024-07-02 21:47:38 -0500

    Time: 1087 ms (18.38%) | Memory: 27.3 MB (65.20%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,20 +1,26 @@
-# Approach 1
+# Approach 2
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.subMatrix = []
+        self.subMatrix = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
         for i in range(len(matrix)):
-            total = 0
-            sublist = []
             for j in range(len(matrix[0])):
-                total += matrix[i][j]
-                sublist.append(total)
-            print(sublist)
-            self.subMatrix.append(sublist)
-                
+                if i > 0:
+                    self.subMatrix[i][j] += self.subMatrix[i-1][j]
+                if j > 0:
+                    self.subMatrix[i][j] += self.subMatrix[i][j-1]
+                if i>0 and j>0:
+                    self.subMatrix[i][j] -= self.subMatrix[i-1][j-1]
+                self.subMatrix[i][j] += matrix[i][j]
+        print(self.subMatrix)  
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         s = 0
-        for i in range(row1,row2+1):
-            s += self.subMatrix[i][col2] - self.subMatrix[i][col1-1] if col1 > 0 else self.subMatrix[i][col2]
+        s += self.subMatrix[row2][col2]
+        if col1>0 and row1>0:
+            s += self.subMatrix[row1-1][col1-1]
+        if col1 > 0:
+            s -= self.subMatrix[row2][col1-1]
+        if row1 > 0:
+            s -= self.subMatrix[row1-1][col2]
         return s
