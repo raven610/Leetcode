0d6edadfed38fe821ba7bb87670eb821d2cc2be3
Pavan Kumar <32418907+raven610@users.noreply.github.com>
2024-07-04 00:27:02 -0500

    Time: 104 ms (15.19%) | Memory: 21.4 MB (30.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,22 +1,4 @@
 class Solution:
-    def isAnagram(self,s: str, t: str) -> bool:
-        d = {}
-        for i in s:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
-        for i in t:
-            if i in d:
-                d[i] -= 1
-                if d[i] == 0:
-                    del d[i]
-            else:
-                return False
-        if len(d.keys()) == 0:
-            return True
-        return False
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d1 = defaultdict(list)
         for s in strs:
@@ -25,4 +7,3 @@ class Solution:
                 l[ord(i)-ord('a')] += 1
             d1[tuple(l)].append(s) 
         return [i for i in d1.values()]
-        
