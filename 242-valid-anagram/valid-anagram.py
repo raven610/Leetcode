d6142095005d195579a7e46f428e24c836e5a23a
Pavan Kumar <32418907+raven610@users.noreply.github.com>
2024-07-04 00:24:02 -0500

    Time: 51 ms (47.51%) | Memory: 16.9 MB (47.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,13 @@
-#approach 1
+#approach 2
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d = {}
+        c1 = [0]*26
+        c2 = [0]*26
         for i in s:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
+            c1[ord(i)-ord('a')] += 1
         for i in t:
-            if i in d:
-                d[i] -= 1
-                if d[i] == 0:
-                    del d[i]
-            else:
+            c2[ord(i)-ord('a')] += 1
+        for i in range(26):
+            if c1[i] != c2[i]:
                 return False
-        if len(d.keys()) == 0:
-            return True
-        return False
+        return True
