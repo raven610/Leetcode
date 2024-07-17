98e216774495df13e3380f1ffd4389e5e1ae8e41
Pavan Kumar <32418907+raven610@users.noreply.github.com>
2024-07-16 22:52:26 -0500

    Time: 37 ms (38.74%) | Memory: 16.6 MB (6.74%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -4,7 +4,6 @@ class Solution:
         s = ''
         while columnNumber > 0:
             c = chr((columnNumber-1)%26 + 65)
-            print(math.ceil((columnNumber-1)%26 + 65))
             s = c + s
             columnNumber = (columnNumber -1) //26
         return s
